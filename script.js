b7c6c493d29c64619a291850a926d5bd959a388a
Carlo Santos <rcarlosantos89@gmail.com>
2024-02-06 10:28:32 -0600

    initial commit: starting a study guide with charles

---
+++ script.js
@@ -0,0 +1,22 @@
+let shopping = [
+  {
+    id: 1,
+    name: "tomatoes",
+    price: 1.00,
+    quantity: 5,
+  },
+  {
+    id: 1,
+    name: "tomatoes",
+    price: 1.00,
+    quantity: 5,
+  },
+  {
+    id: 1,
+    name: "tomatoes",
+    price: 1.00,
+    quantity: 5,
+  }
+]
+
+
